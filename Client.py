565789e75932dc32dcdb27046b2ba41650a28c5e
prisoner1 <88316439+dandan59832@users.noreply.github.com>
2026-05-15 00:07:34 +0300

    updated logic

--- Client.py
+++ Client.py
@@ -1,11 +1,8 @@
 import socket
 import time
-from http import client
-from locale import currency
-from unittest import result
-from urllib import request
 
 from MinigamesHackathon2026.Interface import password
+
 player_id = 0
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 8080))
@@ -17,35 +14,43 @@ if(client):
     player_id = sock.recv(1024)
 
 
-gameRunning = False
-while(gameRunning == False):
-    state = sock.recv(1024).decode('utf-8')
-    if(state == "running"):
-        gameRunning = True
 def shopping():
-    money = sock.recv(1024)
-    displayLeaderBoard(money)
+    money = sock.recv(1024).decode().strip().upper()
+    display_money(money)
     currency = money[player_id]
     purchase = shopping(currency)
-    bid = purchase[0]
-    purchase.pop(0)
+
     sock.send(purchase)
-    return bid
 
 
-def miniGame(int id):
-
+def miniGame(id):
+    run_mini_game(id)
 
 
 
 while (True):
-    result = miniGame(sock.recv(1024).decode(int))
-    sock.send(str(result).encode('utf-8'))
-    time.sleep(1)
-    result = sock.recv(1024)
-    show_result(result)
+    state = sock.recv(1024).decode('utf-8')
+    if(state == "waiting"):
+        continue
+    if(state == "start minigame"):
+        result = miniGame(sock.recv(1024).decode().strip().upper())
+        sock.send(result)
+        continue
+    if(state == "waiting for result"):
+        continue
 
-    bid = shopping()
+    if(state == "leaderboards"):
+        placement = sock.recv(1024).decode().strip().upper()
+        if placement != state:
+            display_leaderboards(result)
+        time.sleep(5)
+    if(state == "buy phase"):
+        shopping()
+        continue
+    if(state == "game finished"):
+        break
 
-
+time.sleep(1)
+end_result = sock.recv(1024).decode().strip().upper()
+game_over(end_result)
 
